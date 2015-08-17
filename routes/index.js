ed18afe4e27f7eb78126f129490fe368792ef302
rociodemula <rociodemula@gmail.com>
2015-08-17 12:31:49 +0200

    P2P M7

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,12 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load); //autoload :quizId
+
 //Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
-//router.get('/quizes/question', quizController.question);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 router.get('/author', function(req, res) {
