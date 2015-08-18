110965269f1fd7e693c3adaade31230fff66ef57
rociodemula <rociodemula@gmail.com>
2015-08-18 10:21:38 +0200

    Comentarios

--- models/models.js
+++ models/models.js
@@ -29,7 +29,16 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 //Importar la definición de la tabla Quiz en quiz.js
 var quiz_path = path.join(__dirname, 'quiz');
 var Quiz = sequelize.import(quiz_path);
+
+//Importar la definición de la tabla Comment en comment.js
+var comment_path = path.join(__dirname, 'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; //exportar definición de tabla Quiz
+exports.Comment = Comment;
 
 // sequilize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function(){
